7b5fbbf3ff7bf9e1bdee65b1b21118aa90a32b82
1XSTRY <kosogornov99@gmail.com>
2023-09-17 15:05:01 +0500

    Found the difference in two ways Also added a division by tasks

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,34 @@
 public class Main {
     public static void main(String[] args) {
+        //task 1
         var dog = 0.8;
         var cat = 0.6;
         var paper = 763789;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        //task 2
         dog = dog + 4;
         cat = cat + 4;
         paper = paper +4;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        //task 3
         dog = dog - 3.5;
         cat = cat -1.6;
         paper = paper - 7639;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        //task 4
         var friend = 19;
         System.out.println(friend);
         friend = friend - 2;
         System.out.println(friend);
         friend = friend / 7;
         System.out.println(friend);
+        //task 5
         var frog = 3.5;
         System.out.println(frog);
         frog = frog * 10;
@@ -32,12 +37,20 @@ public class Main {
         System.out.println(frog);
         frog = frog + 4;
         System.out.println(frog);
-        var firsBoxer = 78.2;
+        //task 6
+        var firstBoxer = 78.2;
         var secondBoxer = 82.7;
-        var twoBoxers = firsBoxer + secondBoxer;
+        var twoBoxers = firstBoxer + secondBoxer;
         System.out.println("wight two boxers = " + twoBoxers);
-        var difference = secondBoxer - firsBoxer;
+        var difference = secondBoxer - firstBoxer;
         System.out.println("the difference between the two boxers is " + difference);
+        //task 7
+        var resultSubtraction = secondBoxer - firstBoxer;
+        System.out.println(resultSubtraction);
+        var resultRemainder = secondBoxer % firstBoxer;
+        System.out.println(resultRemainder);
+
+
 
 
 
